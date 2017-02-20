eca38b16cdc5bfb17825fc2fc74321a9fcfb5398
EKC3\10193157 <397383523@qq.com>
2017-02-20 10:09:22 +0800

    本地进行修改

--- app/src/main/java/com/wj/AppConstant.java
+++ app/src/main/java/com/wj/AppConstant.java
@@ -10,7 +10,7 @@ public class AppConstant {
 
 
     public static class PlayerMsg {
-        public static final int PLAY_MSG = 1;
+        public static final int PLAY_MSG = 18;
         public static final int PAUSE_MSG = 2;
         public static final int STOP_MSG = 3;
         public static final int CONTINUE_MSG = 4;
