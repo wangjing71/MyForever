fb37484c0eebb699f44fc7ecd7aaccd196b58cb0
Wiggins.Wang <397383523@qq.com>
2017-02-20 14:36:12 +0800

    22222222222222

--- app/src/main/java/com/wj/AppConstant.java
+++ app/src/main/java/com/wj/AppConstant.java
@@ -10,7 +10,7 @@ public class AppConstant {
 
 
     public static class PlayerMsg {
-        public static final int PLAY_MSG = 11111111;
+        public static final int PLAY_MSG = 222222222222;
         public static final int PAUSE_MSG = 2;
         public static final int STOP_MSG = 3;
         public static final int CONTINUE_MSG = 4;
