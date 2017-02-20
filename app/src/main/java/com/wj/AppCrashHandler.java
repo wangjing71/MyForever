9fa37cb88ced08d328bf3c476f93a3019f1b0ffc
Wiggins.Wang <397383523@qq.com>
2017-02-20 14:16:58 +0800

    Update AppCrashHandler.java

--- app/src/main/java/com/wj/AppCrashHandler.java
+++ app/src/main/java/com/wj/AppCrashHandler.java
@@ -19,7 +19,7 @@ public class AppCrashHandler implements UncaughtExceptionHandler {
 		defaultCrashHandler = Thread.getDefaultUncaughtExceptionHandler();
 		this.context = context;
 	}
-	
+	//test
 	@Override
 	public void uncaughtException(Thread thread, Throwable ex) {
 		Log.i(TAG, "App crashed by uncaughtException", ex);
