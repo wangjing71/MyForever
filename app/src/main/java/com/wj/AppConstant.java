cb9b188c49ba1c1fd3e84a961ac246957ae440bf
EKC3\10193157 <397383523@qq.com>
2017-03-09 13:36:15 +0800

    Merge remote-tracking branch 'remotes/origin/master'
    # Conflicts:
    #	app/src/main/java/com/wj/AppConstant.java

--- app/src/main/java/com/wj/AppConstant.java
+++ app/src/main/java/com/wj/AppConstant.java
@@ -11,7 +11,7 @@ public class AppConstant {
 
 
     public static class PlayerMsg {
-        //333333333333333
+        //333333333333333 55555555555555
         public static final int PLAY_MSG = 999999999;
         public static final int PAUSE_MSG = 23424;
         public static final int STOP_MSG = 333;
